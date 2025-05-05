0bd26f4c6e3a6d3699263f81fd08f2450e6e2d6b
Pau Enciso Bosch <pauenbosch@gmail.com>
2025-05-05 17:01:48 +0200

    Canvis en URL a server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://anitrack-sable.vercel.app/', 'http://localhost:3000']
+  origin: ['https://anitrack-sable.vercel.app', 'http://localhost:3000']
 }));
 app.use(express.json());
 
